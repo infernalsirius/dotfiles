326fe5ac09748e0fabad2eeacde1018ebc270e25
Patrick Meunier <patrick@tekktonaut.com>
2025-05-12 20:08:15 -0400

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,15 +1,10 @@
-if status is-interactive
-  mise activate fish | source
-else
-  mise activate fish --shims | source
-end
-
 # Initialize atuin
 atuin init fish | source
 
 ### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
 set --export --prepend PATH "/Users/patrick/.rd/bin"
 ### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
+
 source /Users/patrick/.config/op/plugins.sh
 
 # bun
@@ -27,10 +22,6 @@ function starship_transient_prompt_func
 end
 starship init fish | source
 enable_transience
-
-# tabtab source for packages
-# uninstall by removing these lines
-[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
 
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
